9470987047827bbd823e8d8fe915196bbd21001b
nikolozi <nikolozi0525@gmail.com>
2024-05-22 17:09:00 +0400

    fix J: place logo on the home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { MDBIcon, MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
 import React from "react";
 import { Link } from "react-router-dom";
 
+import HomePageLogo from "src/assets/homepage.png";
+
 import "./App.css";
 
 export default function App() {
@@ -9,14 +11,24 @@ export default function App() {
     <MDBContainer fluid className="start-page ">
       <MDBRow className="start-page-main">
         <MDBCol className="start-page-title" md={7} sm={12}>
-          <div>
+          <div className="text-center">
+            <div>
+              <img
+                src={HomePageLogo}
+                alt="Bible"
+                style={{ width: "10vh", height: "auto" }}
+              />
+            </div>
             <div className="">B i b l e</div>
             <div className="">V e r s e</div>
             <div className="">M a s t e r</div>
           </div>
         </MDBCol>
-        <MDBCol md={1} sm={12}></MDBCol>
-        <MDBCol className="d-flex align-items-center" md={4} sm={12}>
+        <MDBCol
+          className="d-flex align-items-center justify-content-center"
+          md={5}
+          sm={12}
+        >
           <Link to="/before">
             <div className="start-page-content">
               S T A R T
